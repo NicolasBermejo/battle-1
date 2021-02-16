b46693b3ff364f59c95b458762c22efaebc9434f
Anna Nawrocka <anna.nawrocka1988@gmail.com>
2021-02-16 17:41:12 +0000

    app.rb modified

--- app.rb
+++ app.rb
@@ -1,23 +1,6 @@
 require 'sinatra'
 require 'shotgun'
-get '/' do
-  "Hello!"
-end
-
-get '/secret' do
-  "shhhhh!!"
-end
-
-get '/batcave' do 
-  "this is batcave"
-end
-
-get '/death' do 
-  "this is the end"
-end
 
 get '/cat' do 
-  "<div>
-  <img src=https://www.catster.com/wp-content/uploads/2018/01/Ragdoll-cat-on-blue-backdrop-.jpg>
-  </div>"
+  erb(:index)
 end 
