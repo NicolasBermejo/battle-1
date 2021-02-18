783f2e4cbdc0276d925282313265560dc75fc958
Nicolas Bermejo <nicbermejo@gmail.com>
2021-02-18 18:54:16 +0100

    made the controlers skinny with game global variable

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@
 
 require 'sinatra/base'
 require './lib/Player.rb'
+require './lib/Game.rb'
 class Battle < Sinatra::Base
 
   enable :sessions
@@ -12,21 +13,20 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $player1 = Player.new(params[:player1])
-    $player2 = Player.new(params[:player2])
+    player1 = Player.new(params[:player1])
+    player2 = Player.new(params[:player2])
+    $game = Game.new(player1, player2)
     redirect '/play'
   end
 
   get '/play' do
-    @player1 = $player1
-    @player2 = $player2
+    @game = $game
     erb(:play)
   end
 
   get '/attack' do
-    @player1 = $player1
-    @player2 = $player2
-    @player1.attack(@player2)
+    @game = $game
+    @game.attack(@game.player2)
     erb(:attack)
   end
   # start the server if ruby file executed directly
--- spec/Game_spec.rb
+++ spec/Game_spec.rb
@@ -1,5 +1,14 @@
 require './lib/Game.rb'
 
 describe Game do
-  
+  subject(:subject) { Game.new('Ian', 'Anna') }
+  describe '#attack' do
+    let(:player1) {double :player}
+    let(:player2) {double :player}
+
+    it 'damages the player' do
+      expect(player2).to receive(:receive_damage)
+      subject.attack(player2)
+    end
+  end
 end
