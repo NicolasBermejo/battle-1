2e66eeff53aba2ce169f133e9d36765e7b9760e1
Anna Nawrocka <anna.nawrocka1988@gmail.com>
2021-02-18 09:23:43 +0000

    Add test infrastructure and initial setup

--- Gemfile
+++ Gemfile
@@ -4,8 +4,8 @@ source "https://rubygems.org"
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
-# gem "rails"
+gem 'rspec'
 gem 'sinatra'
-gem 'shotgun'
 gem 'capybara'
-gem 'selenium-webdriver'
+gem 'shotgun'
+# gem "rails"
--- app.rb
+++ app.rb
@@ -1,17 +1,45 @@
-require 'sinatra'
-require 'shotgun'
+# in app.rb
 
-get '/random-cat' do
-  @name = ["Amigo", "Speedy", "Sven"].sample 
-  erb(:index)
-end 
+require 'sinatra/base'
 
-post '/named-cat' do 
-  p params
-  @name = params[:name] 
-  erb(:index)
-end 
+class Battle < Sinatra::Base
+  get '/' do
+      # p params
+      # @catname = params[:name]
+      erb(:index)
+  end
 
-get '/cat-form' do 
-  erb(:cat_form)
+  post '/names' do
+    @name1 = params[:name1]
+    @name2 = params[:name2]
+    erb(:play)
+  end
+
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
 end
+#
+# get '/secret' do
+#   "Shhhhhhhhhhhh I'm hunting duckies"
+# end
+#
+# get '/random-cat' do
+#   @catname = %w(Amigo Misty Almond).sample
+#   erb(:index)
+# end
+#
+# post '/named-cat' do
+#   p params
+#   @catname = params[:name]
+#   erb(:index)
+# end
+#
+# get '/post-cat' do
+#   # p params
+#   # @catname = params[:name]
+#   erb(:forms)
+# end
+#
+
+#test comment
